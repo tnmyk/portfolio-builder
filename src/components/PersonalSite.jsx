5b11b4445a3fa9839ecf1918578757d164a230bc
Tanmaykk <tanmaykachroo@gmail.com>
2021-06-28 13:34:24 +0530

    ok

--- src/components/PersonalSite.jsx
+++ src/components/PersonalSite.jsx
@@ -5,29 +5,26 @@ import Error from "./Error";
 import PersonalNav from "./personalComponents/PersonalNav";
 import SocialBtns from "./personalComponents/SocialBtns";
 const PersonalSite = ({ setPersonal }) => {
-  const { username } = useParams('');
+  const { username } = useParams("");
   const [data, setData] = useState("");
   const [loading, setLoading] = useState(true);
-  const [notFound,setNotFound] =useState(false)
+  const [notFound, setNotFound] = useState(false);
   useEffect(() => {
-
     setPersonal(true);
     db.collection("users")
       .where("username", "==", username)
       .get()
       .then((docSnapshot) => {
         if (docSnapshot.empty) {
-          console.log("no user");
-          setNotFound(true)
-          setPersonal(false)
+          setNotFound(true);
+          setPersonal(false);
           return;
         }
         require("../css/personalSite.css");
         setData(docSnapshot.docs[0].data());
         setLoading(false);
-
       });
-  }, []);
+  }, [username,setPersonal]);
 
   const Site = () => {
     return (
@@ -37,22 +34,25 @@ const PersonalSite = ({ setPersonal }) => {
           <div className="intro-flex">
             <div className="intro-photo">
               <div className="photo-container">
-                <img src={data.data.photo} alt='' className='personal-photo'/>
+                <img src={data.data.photo} alt="" className="personal-photo" />
               </div>
             </div>
             <div className="intro-text">
-              <h3 className='personal-intro'>{data.data.form.intro}</h3>
-              <p className='personal-bio'>{data.data.form.bio}</p>
-              <SocialBtns socialLinks={data.data.socialLinks}/>
+              <h3 className="personal-intro">{data.data.form.intro}</h3>
+              <p className="personal-bio">{data.data.form.bio}</p>
+              <SocialBtns socialLinks={data.data.socialLinks} />
             </div>
           </div>
         </div>
       </div>
     );
   };
-  return (<>{!loading && <Site />}
-  {notFound && <Error/>}
-  </>);
+  return (
+    <>
+      {!loading && <Site />}
+      {notFound && <Error />}
+    </>
+  );
 };
 
 export default PersonalSite;
