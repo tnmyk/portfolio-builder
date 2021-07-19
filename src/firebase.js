b2dab4d854e461150793a41b7b93b1a8dedad525
Tanmaykk <tanmaykachroo@gmail.com>
2021-07-20 03:04:27 +0530

    ok

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,14 @@ import 'firebase/firestore'
 import "firebase/storage";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDEi9yxsDuzZ_FTGipbNNEHxfVKOvi5qfc",
-  authDomain: "portfolio-builder-3dc59.firebaseapp.com",
-  projectId: "portfolio-builder-3dc59",
-  storageBucket: "portfolio-builder-3dc59.appspot.com",
-  messagingSenderId: "1064984824115",
-  appId: "1:1064984824115:web:aaa78ab9e33e66bf5ad646",
-  measurementId: "G-G03KZCZE8S",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
-
 const app = firebase.initializeApp(firebaseConfig);
 
 export const db = app.firestore()
